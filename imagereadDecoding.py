79c4111af6fbafa5d478d1d03e8253f787f5f609
RedaVaisetaite <36488401+RedaVaisetaite@users.noreply.github.com>
2019-05-05 00:46:59 +0300

    pradėtas daryti gui. Padarytas dekodavimo langas

--- imagereadDecoding.py
+++ imagereadDecoding.py
@@ -1,18 +1,65 @@
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QPixmap
+from PyQt5.uic import loadUi
 import numpy as np
+import os
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
 import colorsys
 from PIL import Image
 import sys
+class Window(QtWidgets.QMainWindow):
 
+    def __init__(self):
+        super(Window, self).__init__()
+        loadUi('gui.ui',self)
+        self.setWindowTitle('Erdvę užpildančios kreivės')
+        self.decode.clicked.connect(self.decodeButton_clicked)
+        self.browse.clicked.connect(self.getImage)
+    @pyqtSlot()
+    def decodeButton_clicked(self):
+        
+        textLength = self.textLength.value()
+        imagePath = self.imagepath.text()
+        img_file = Image.open(imagePath)
+
+        img = img_file.resize((256, 256), Image.ANTIALIAS)
+
+        img_resized = img.load() #uzsikraunam pikselius
+        N = 256
+        decodedText = []
+        it = 0
+        for i in range(0,N*N): 
+      #ir dar vietoj texto padavineti teksto ilgi, kuri gausim is parametru      
+          if (it < textLength * 4):
+            letter = hilbert2xy(i, N, textLength, 2, it, img_resized)          
+            m = it          
+    #print(decodedText)
+            if (it >= textLength):
+              m = it%textLength
+              decodedText[m] = decodedText[m] + letter
+            else:
+              decodedText.append(letter)        
+          it = it + 1
+  
+        if (decodedText != []):
+          text = ''
+          for letter in decodedText:
+            text = text + chr(letter)
+        self.label2.setText(text)
+    def getImage(self):
+      fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file',
+                            'c:\\', "Image files (*.jpg *.jpeg, *.png)")
+      imagePath = fname[0]
+      imageName = imagePath.split("/")[-1]
+      #pixmap = QPixmap(imagePath)
+      #self.label2.setPixmap(QPixmap(pixmap))
+      self.imagename.setText(imageName)
+      self.imagepath.setText(imagePath)
+      #self.resize(pixmap.width(), pixmap.height())
 #tk.Spinbox()
-imageName = sys.argv[1]
-textLength = int(sys.argv[2])
-img_file = Image.open(imageName)
 
-img = img_file.resize((256, 256), Image.ANTIALIAS)
-
-img_resized = img.load() #uzsikraunam pikselius
 
 def getSum(a, b):
     while b:
@@ -20,7 +67,7 @@ def getSum(a, b):
     return a
 
 # (2) Get image width & height in pixels
-[xs, ys] = img.size    
+#[xs, ys] = img.size    
 
 #check if the color is not out of limit
 #def clamp(x): 
@@ -29,6 +76,8 @@ def getSum(a, b):
 #encoding function
 
 def decoding(b, it, bits, textLength):
+  #textLength = int(sys.argv[2])
+
   #kol kart 4, nes darom kad keiciam po du bitukus ir tada turim kiekvienai
   #raidei/char 4 poras.
   bb = (b & 3)
@@ -53,7 +102,7 @@ def decoding(b, it, bits, textLength):
 def last2bits(hindex):
   return (hindex & 3)
 def rshift(val, n): return (val % 0x100000000) >> n
-def hilbert2xy(hindex, N, textLength, bits, it):
+def hilbert2xy(hindex, N, textLength, bits, it, img_resized):
   positions = [
     [0, 0],
     [0, 1],
@@ -94,28 +143,31 @@ def hilbert2xy(hindex, N, textLength, bits, it):
       
     
 
-N = 256
-decodedText = []
+
  #turi buti argv
 #print(textLength)
-it = 0
-for i in range(0,N*N): 
+#it = 0
+app = QtWidgets.QApplication(sys.argv)
+gui = Window()
+gui.show()
+sys.exit(app.exec_())
+#for i in range(0,N*N): 
       #ir dar vietoj texto padavineti teksto ilgi, kuri gausim is parametru      
-  if (it < textLength * 4):
-    letter = hilbert2xy(i,N, textLength, 2, it)          
-    m = it          
+#  if (it < textLength * 4):
+#    letter = hilbert2xy(i,N, textLength, 2, it)          
+#    m = it          
     #print(decodedText)
-    if (it >= textLength):
-      m = it%textLength
-      decodedText[m] = decodedText[m] + letter
-    else:
-      decodedText.append(letter)        
-  it = it + 1
+ #   if (it >= textLength):
+  #    m = it%textLength
+   #   decodedText[m] = decodedText[m] + letter
+    #else:
+     # decodedText.append(letter)        
+  #it = it + 1
   
-if (decodedText != []):
-    text = ''
-    for letter in decodedText:
-        text = text + chr(letter)
-    print(text)
+#if (decodedText != []):
+ #   text = ''
+  #  for letter in decodedText:
+   #     text = text + chr(letter)
+    #print(text)
     #print(decodedText)
 #img.save("test.png")
